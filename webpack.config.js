ce0a1b4a2377963e08096af518ed326727f9787d
Yi-An Chen <chenyian@nibiohn.go.jp>
2021-09-14 17:40:40 +0900

    intial commit - scaffolded tool via CLI

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: "bluegenesCompoundStructureImage",
+    libraryTarget: "var"
+  },
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      
+    ]
+  }
+};
